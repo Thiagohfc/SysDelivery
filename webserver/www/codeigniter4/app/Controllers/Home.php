b6eaea654cafaa6ec1862e9f7f14137dd898a5be
Thiago Henrique Felix Cândido Ribeiro Conceição <116850972+Thiagohfc@users.noreply.github.com>
2025-05-07 16:20:52 -0300

    Add: Migrations e mudanças na Home

--- webserver/www/codeigniter4/app/Controllers/Home.php
+++ webserver/www/codeigniter4/app/Controllers/Home.php
@@ -1,13 +1,38 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\Produtos as Produtos_model;
+use App\Models\Categorias as Categorias_model;
+use App\Models\Imgprodutos as Imgprodutos_model;
 
 class Home extends BaseController
 {
+    private $produtos;
+    private $categorias;
+    private $imagens;
+
+    public function __construct(){
+        $this->produtos = new Produtos_model();
+        $this->categorias = new Categorias_model();
+        $this->imagens = new Imgprodutos_model();
+        helper('functions');
+    }
+
+
     public function index()
     {   
         $data['titulo'] = "Home";
-        // $data['conteudo'] = "Seja bem vindo ao SYS Delivery!";
+        $categorias = $this->categorias->findAll();
+        $data['all_produtos'] = []; 
+
+        for($i=0; $i < count($categorias);$i++){
+            $categoria = array(
+                "categorias_id" => $categorias[$i]->categorias_id,
+                "categorias_nome" => $categorias[$i]->categorias_nome,
+                "produtos" => $this->produtos->join('imgprodutos', 'imgprodutos_produtos_id = produtos_id')->join('categorias', 'produtos_categorias_id = categorias_id')->where('categorias_id', (int) $categorias[$i]->categorias_id)->find()
+            );
+            $data['all_produtos'][$i] = $categoria;
+        };
         return view('home/index',$data);
     }
 
--- webserver/www/codeigniter4/app/Views/home/index.php
+++ webserver/www/codeigniter4/app/Views/home/index.php
@@ -5,7 +5,7 @@
 <!--Abre Produtos-->
 <div id="produtos" class="container">
 
-<h1 class="col mt-3">Produtos</h1>
+<h2 class="border-bottom mt-3 border-2 border-primary">Produtos</h1>
 
 <div class="col mt-3 mb-3">
     <form class="d-flex" role="search">
@@ -17,32 +17,43 @@
     </form>
 </div>
 
-<div class="row">
-
 <?php 
-$produtos = ['hamburguer1.png','hamburguer2.png','hamburguer3.png','hamburguer4.png','hamburguer5.png','pizza1.png','pizza2.png','pizza3.png','pizza4.png'];
-for($i=0; $i < count($produtos);$i++){
 
-?>
-    <!-- card 1 -->
-    <div class="col-sm-3 mb-3 pb-4 mb-sm-0">
-        <div class="card">
-            <img src="<?= base_url('assets/images/'.$produtos[$i]) ?>" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title"><?= $produtos[$i] ?> </h5>
-                <h5 class="card-title"><b class="text-danger"> R$ 50,00 </b></h5>
-                <p class="card-text ">With supporting text below as a
-                    natural lead-in to additional content.</p>
-                <p class="text-center"><a href="#" class="btn btn-primary">Comprar <i class="bi bi-basket2-fill"></i></a></p>
+for($i=0; $i < count($all_produtos); $i++){
+?>  
+    <h1><?= $all_produtos[$i]['categorias_nome']?></h1> 
+    
+    <div class="row">
+
+
+        <?php 
+        if(empty($all_produtos[$i]['produtos'])){
+            echo '<div class="col mb-3 pb-4 mb-sm-0">
+                    Ainda não há produtos cadastrados para esta categoria!
+                </div>';
+        }else{
+
+        foreach($all_produtos[$i]['produtos'] as $produto){
+
+        ?>
+            <!-- card 1 -->
+            <div class="col-sm-3 mb-3 pb-4 mb-sm-0">
+                <div class="card">
+                    <img src="<?= base_url('assets/'.$produto->imgprodutos_link) ?>" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $produto->produtos_nome; ?> </h5>
+                        <h5 class="card-title"><b class="text-danger"> R$ <?= $produto->produtos_preco_venda; ?> </b></h5>
+                        <p class="card-text "><?= $produto->produtos_descricao ?></p>
+                        <p class="text-center"><a href="#" class="btn btn-primary">Comprar <i class="bi bi-basket2-fill"></i></a></p>
+                    </div>
+                </div>
             </div>
-        </div>
+
+        <?php } }?>
+
     </div>
 
-
-
 <?php } ?>
-
-</div>
 
 </div>
 
