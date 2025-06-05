1c06371238e4fa2ce7b947a75edc2eaabe11b897
Orlando Filho <orlandossf04@gmail.com>
2025-06-04 21:59:16 -0300

    footer sempre embaixo

--- webserver/www/codeigniter4/app/Views/Templates_user.php
+++ webserver/www/codeigniter4/app/Views/Templates_user.php
@@ -1,7 +1,27 @@
-<?php 
-    include('templates/header.php');
-    include('templates/nav_user.php');
-    echo $this->renderSection('content');
-    include('templates/footer.php');
-    include('templates/end.php');
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php include('templates/header.php'); ?>
+</head>
+
+<body>
+    <!-- Estrutura principal com flexbox para ocupar toda a altura da tela -->
+    <div class="d-flex flex-column min-vh-100">
+
+        <!-- Navegação do usuário -->
+        <?php include('templates/nav_user.php'); ?>
+
+        <!-- Conteúdo principal -->
+        <main class="flex-fill">
+            <?= $this->renderSection('content'); ?>
+        </main>
+
+        <!-- Rodapé -->
+        <?php include('templates/footer.php'); ?>
+    </div>
+
+    <?php include('templates/end.php'); ?>
+</body>
+
+</html>
